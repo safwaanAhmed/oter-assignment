044d2d8b03a99b687182543728ec1240bf0a5ae1
Safwaan Ahmed <safwaan@doodleblue.com>
2023-06-12 15:02:27 +0530

    Remove dist folder

--- dist/utils/common.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.getFullName = void 0;
-const getFullName = (firstName, lastName) => {
-    return `${firstName.trim()} ${lastName.trim()}`;
-};
-exports.getFullName = getFullName;
