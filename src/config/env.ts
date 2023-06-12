1303c842fc2af8f11c6e4980780bdf1773b44ccb
Safwaan Ahmed <safwaan@doodleblue.com>
2023-06-12 13:42:53 +0530

    dotenv

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,14 +1,16 @@
 import dotenv from 'dotenv';
 
-const result = dotenv.config();
+if (process.env.NODE_ENV !== 'production') {
+  const result = dotenv.config();
 
-if (result.error) {
-  console.error('Error loading .env file:', result.error);
-  process.exit(1);
+  if (result.error) {
+    console.error('Error loading .env file:', result.error);
+    process.exit(1);
+  }
 }
 
 export default {
-    PORT: process.env.PORT,
-    NODE_ENV: process.env.NODE_ENV,
-    MONGO_URI: process.env.MONGO_URI
+  PORT: process.env.PORT,
+  NODE_ENV: process.env.NODE_ENV,
+  MONGO_URI: process.env.MONGO_URI
 }
